7ad37d4de37009822a6bbb83077940104d1a29bc
egjarabo <e.glez.telefonica@gmail.com>
2020-09-23 07:50:09 +0200

    Initial commit

---
+++ src/main/java/com/kike/VendorRestApplication.java
@@ -0,0 +1,13 @@
+package com.kike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendorRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kike/VendorRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
